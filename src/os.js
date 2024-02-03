9b34e3f017da76ac6dce2ed089598c3223006608
Maria T <bananazigzags@gmail.com>
2024-02-03 20:06:18 +0300

    refactor os

--- src/os.js
+++ src/os.js
@@ -12,24 +12,12 @@ const getCpuInfoOutput = (info) => {
   return `There are ${cpuNum} CPUs.\n${cpuInfo}`;
 };
 
-export const os = async (command) => {
-  switch (command) {
-    case "EOL":
-      const eol = JSON.stringify(EOL);
-      return `Default system end-of-line is ${eol}`;
-    case "cpus":
-      const cpuInfo = cpus();
-      return getCpuInfoOutput(cpuInfo);
-    case "homedir":
-      const homeDir = homedir();
-      return `Home directory is ${homeDir}`;
-    case "username":
-      const name = userInfo().username;
-      return `Current system username is ${name}`;
-    case "architecture":
-      const archInfo = arch();
-      return `CPU architecture for which Node.js binary has compiled is ${archInfo}`;
-    default:
-      return OS_COMMAND_OPTIONS_MSG;
-  }
-};
+const osHandlersMap = {
+  EOL: () => `Default system end-of-line is ${JSON.stringify(EOL)}`,
+  cpus: () => getCpuInfoOutput(cpus()),
+  homedir: () => `Home directory is ${homedir()}`,
+  username: () => `Current system username is ${userInfo().username}`,
+  architecture: () => `CPU architecture for which Node.js binary has compiled is ${arch()}`,
+}
+
+export const os = (command) => osHandlersMap[command]?.() || OS_COMMAND_OPTIONS_MSG
